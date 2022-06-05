480a62702959218f1d4586affc1cfffffb8cda0d
artavasdes <vartanyildiz@gmail.com>
2022-06-05 10:40:34 -0400

    Started work on NFT bridging

---
+++ Assets/Scripts/CharactersView/BuyCharacterButtonScript.cs
@@ -0,0 +1,11 @@
+  using UnityEngine;
+  using System.Collections;
+  
+  public class BuyCharacterButtonScript : MonoBehaviour
+  {
+      public void OpenURL()
+      {
+          Application.OpenURL("https://testnets.opensea.io/assets/unidentified-contract-csapfibmkh");
+      }
+  }
+
--- Assets/Scripts/MenuScreen/QuitButton.cs
+++ Assets/Scripts/MenuScreen/QuitButton.cs
@@ -1,19 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
 public class QuitButton : MonoBehaviour
 {
-    public UnityEvent buttonClick;
-
-    void Awake(){
-        if(buttonClick == null){
-            buttonClick = new UnityEvent();
-        }
-    }
-
-    void OnMouseDown(){
+    public void QuitGame(){
         Application.Quit();
     }
     
