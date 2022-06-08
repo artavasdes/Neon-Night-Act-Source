edb48814b769d27a5b12b1804089c446e461347a
artavasdes <56280139+artavasdes@users.noreply.github.com>
2022-06-08 11:52:40 -0400

    Added working valid message checker

--- Assets/Scripts/CharactersView/EthereumWallettInputScript.cs
+++ Assets/Scripts/CharactersView/EthereumWallettInputScript.cs
@@ -2,21 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class EthereumWallettInputScript : MonoBehaviour
 {
     public string walletAddress;
+    [SerializeField]
+    public TextMeshProUGUI validMessageChange;
 
-    private Text text;
     public void ReadStringInput(string s){
         if(s.Length == 42){
             Debug.Log("Valid Address");
             walletAddress = s;
             CheckNFTOwner nftOwnerCheck = new CheckNFTOwner();        
             nftOwnerCheck.CheckOwner(walletAddress);
+            validMessageChange.text = "Valid Address";
         }
         else{
-
+            validMessageChange.text = "Enter a Valid Ethereum Address Please";
         }
     }
 }
