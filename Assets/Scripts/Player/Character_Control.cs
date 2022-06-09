075813793cb5cc2eab58992f9dc38d6800e30f86
Safa-Karagoz <78101103+Safa-Karagoz@users.noreply.github.com>
2022-06-08 22:29:56 -0400

    fixed missing objects and added smooth player movement

--- Assets/Scripts/Player/Character_Control.cs
+++ Assets/Scripts/Player/Character_Control.cs
@@ -1,7 +1,7 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class Bandit : MonoBehaviour {
+public class Character_Control : MonoBehaviour {
 
     [SerializeField] float      m_speed = 4.0f;
     [SerializeField] float      m_jumpForce = 7.5f;
