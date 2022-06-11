7b47f0a47bfa6a3910267ebf5e402f8e784e2085
artavasdes <vartanyildiz@gmail.com>
2022-06-11 10:33:14 -0400

    Finished nft connection

--- Assets/Scripts/CharactersView/CheckNFTOwner.cs
+++ Assets/Scripts/CharactersView/CheckNFTOwner.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Numerics;
+using System.Threading.Tasks;
 
 public class CheckNFTOwner
 {
@@ -9,11 +10,19 @@ public class CheckNFTOwner
     public string network = "rinkeby";
     public string contract = "0x5D5220AC4aE1615C6ff524490e3b23988190599A";
     public int tokenIdAmount = 5;
-    public async void CheckOwner(string account)
+    public Dictionary<int, bool> ownerDic = new Dictionary<int, bool>();
+
+    public async Task CheckOwner(string account)
     {
         for(int i = 0; i <= tokenIdAmount; i++){
             BigInteger balanceOf = await ERC1155.BalanceOf(chain, network, contract, account, i.ToString());
-            Debug.Log(balanceOf);
-        }       
+            if(balanceOf == 1){
+                ownerDic.Add(i,true);
+            }
+            else{
+                ownerDic.Add(i, false);
+            }            
+        }    
+        await Task.Yield();
     }
 }
--- Assets/Scripts/CharactersView/EthereumWallettInputScript.cs
+++ Assets/Scripts/CharactersView/EthereumWallettInputScript.cs
@@ -10,12 +10,19 @@ public class EthereumWallettInputScript : MonoBehaviour
     [SerializeField]
     public TextMeshProUGUI validMessageChange;
 
-    public void ReadStringInput(string s){
+    public async void ReadStringInput(string s){
         if(s.Length == 42){
             Debug.Log("Valid Address");
             walletAddress = s;
-            CheckNFTOwner nftOwnerCheck = new CheckNFTOwner();        
-            nftOwnerCheck.CheckOwner(walletAddress);
+            CheckNFTOwner nftOwnerCheck = new CheckNFTOwner();  
+
+            await nftOwnerCheck.CheckOwner(walletAddress);
+
+            Debug.Log("count" + nftOwnerCheck.ownerDic.Count);
+
+            foreach (KeyValuePair<int, bool> kvp in nftOwnerCheck.ownerDic)
+                Debug.Log("Key = {0}, Value = {1}" + kvp.Key + kvp.Value);
+                
             validMessageChange.text = "Valid Address";
         }
         else{
