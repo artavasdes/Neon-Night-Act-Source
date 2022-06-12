73d0a65b727c2eabfc13e22710317a12373dbbae
Safa-Karagoz <78101103+Safa-Karagoz@users.noreply.github.com>
2022-06-11 23:54:21 -0400

    added timer, changed health color design

---
+++ Assets/Scripts/Player/Timer.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+
+    public float TimeLeft; 
+    public bool TimerOn = false; 
+
+    public TextMeshProUGUI TimerText;
+    void Start()
+    {
+        TimerOn = true; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TimerOn)
+        {
+            if (TimeLeft > 0)
+            {
+                updateTimer(TimeLeft);
+                TimeLeft -= Time.deltaTime; 
+            }
+            else 
+            {
+                Debug.Log("Time is Up!");
+                TimeLeft = 0;
+                TimerOn = false; 
+            }
+        }
+    }
+
+    void updateTimer(float currentTime)
+    {
+        currentTime -= 1; 
+        
+        TimerText.text = currentTime.ToString("N0");
+    }
+
+}
